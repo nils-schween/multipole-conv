527a83f1335613906784346af29c7f81c3a434b5
Nils Schween <nils.schween@mpi-hd.mpg.de>
2022-03-07 17:13:24 +0100

    playing around

--- multipole-conv/src/multipole-conv.cpp
+++ multipole-conv/src/multipole-conv.cpp
@@ -5,20 +5,14 @@
 #include "options.h"
 #include "square_matrix.h"
 
-// Type traits
-// template<typename T>
-// struct is_complex : std::false_type {};
-
-// template<typename T>
-// struct is_complex<std::complex<T>> : public std::true_type {};
-
 int main(int argc, char *argv[])
 {
+  using namespace std::complex_literals;
   MPOptions test = MPOptions::complex | MPOptions::normalisation;
 
   std::complex<float> a = 3;
   // std::cout << is_complex<int>() ;
-  
+    
   SquareMatrix<double> A(3);
   A(1,1) = 1;
   A(2,2) = 2;
@@ -27,19 +21,21 @@ int main(int argc, char *argv[])
   std::cout << "A \n";
   A.print();
   std::cout << "\n";
-  SquareMatrix<double> B {A/2};
+  auto B {(2i + 0.3)*A};
   std::cout << "B \n";
   B.print();
-  B.transpose().print();
+  (B/2.).transpose().print();
   
   std::cout << "\n";
   std::cout << "A * B \n";
 
-  SquareMatrix<double> C(3);
-  
-  C = A*B;
+  auto C = A*B;
   C.print();
-  
+
+  SquareMatrix<std::complex<double>> D(3);
+  D(1,1) = 0.3 + 2i;
+  std::cout << "\n";
+  D.conjugate().print();
   std::cout << test;
   std::cout << binomial(3, 3) << "\n";
   return 0;
