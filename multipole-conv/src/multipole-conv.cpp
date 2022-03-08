aef4e26d7eef238e11d03ecad9e1859388c03d4e
Nils Schween <nils.schween@mpi-hd.mpg.de>
2022-03-08 17:59:50 +0100

    only very little today

--- multipole-conv/src/multipole-conv.cpp
+++ multipole-conv/src/multipole-conv.cpp
@@ -1,19 +1,62 @@
 #include <iostream>
 #include <complex>
-#include <type_traits>
+#include <cmath>
+
 #include "math.h"
 #include "options.h"
 #include "square_matrix.h"
+
+constexpr double pi = 3.14159265358979;
+
+template <typename T>
+class Normalisation : public SquareMatrix<T> {
+   public:
+  Normalisation(std::size_t l, MPOptions opt) : SquareMatrix<T> {2 * l + 1}, options{opt}
+    {
+      double factor {0.};
+      if ((MPOptions::complex & opt) != MPOptions::none) {
+	factor = {std::sqrt((2 * l + 1)/(4*pi))};
+	
+      }
+      else {
+	factor = {std::sqrt((2 * l + 1)/(2*pi))};
+	for(std::size_t m = 1; m <= l; ++m) {
+	  double norm_l_m = factor * std::sqrt((std::tgamma(l - m + 1)/std::tgamma(l + m + 1)));
+	  *this->operator()(m, m) = norm_l_m;
+	  *this->operator()(2*l - m, 2*l - m) = norm_l_m;
+	}
+	*this->operator()(0,0) = factor;
+      }
+    }
+  void real_or_complex() {
+    if ((MPOptions::complex & options) != MPOptions::none)
+      std::cout << "Normalisation of complex spherical harmonics" << "\n";
+    else
+      std::cout << "Normalisation of real spherical harmonics" << "\n";
+  }
+  
+  Normalisation inverse();
+   private:
+  MPOptions options;
+};
+
+template <typename T>
+Normalisation<T> Normalisation<T>::inverse() {
+  Normalisation<T> res(*this->dim());
+  for(std::size_t i = 0; i < *this->dim(); ++i)
+    res(i, i) = 1./(*this->operator()(i,i));
+  return res;
+}
 
 int main(int argc, char *argv[])
 {
   using namespace std::complex_literals;
   MPOptions test = MPOptions::complex | MPOptions::normalisation;
 
-  std::complex<float> a = 3;
+  std::complex<double> a = 3. + 1.i;
   // std::cout << is_complex<int>() ;
     
-  SquareMatrix<double> A(3);
+  SquareMatrix<double> A {3};
   A(1,1) = 1;
   A(2,2) = 2;
   A(0,2) = 3;
@@ -21,22 +64,25 @@ int main(int argc, char *argv[])
   std::cout << "A \n";
   A.print();
   std::cout << "\n";
-  auto B {(2i + 0.3)*A};
+  auto B {A*2.};
   std::cout << "B \n";
   B.print();
-  (B/2.).transpose().print();
+  B.transpose().print();
   
   std::cout << "\n";
   std::cout << "A * B \n";
-
+  ((3.)*A).print();
   auto C = A*B;
   C.print();
 
   SquareMatrix<std::complex<double>> D(3);
   D(1,1) = 0.3 + 2i;
+  (D/(2. + 1i)).print();
+
+  (D*A).print();
   std::cout << "\n";
   D.conjugate().print();
   std::cout << test;
-  std::cout << binomial(3, 3) << "\n";
+  std::cout << binomial(5, 3) << "\n";
   return 0;
 }
