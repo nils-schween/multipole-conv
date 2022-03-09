49570bdc44864505765bfd34f432b3a83e6af372
Nils Schween <nils.schween@mpi-hd.mpg.de>
2022-03-09 21:31:14 +0100

    wrap options in namespace mutlitpole-conv

--- multipole-conv/include/options.h
+++ multipole-conv/include/options.h
@@ -1,10 +1,13 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 
-enum class MPOptions { none = 1 << 0,
+namespace multipole_conv {
+enum class MPOptions {
+  none = 1 << 0,
   complex = 1 << 1,
   normalisation = 1 << 2,
-  condon_shortley_phase = 1 << 3 };
+  condon_shortley_phase = 1 << 3
+};
 
 constexpr MPOptions operator|(MPOptions f1, MPOptions f2) {
   return static_cast<MPOptions>(static_cast<int>(f1) | static_cast<int>(f2));
@@ -19,9 +22,9 @@ inline StreamType &operator<<(StreamType &s, MPOptions f) {
   s << "Used options: \n";
   if ((f & MPOptions::complex) != MPOptions::none)
     s << "	 - Complex spherical harmonics\n";
-  if ((f & MPOptions::normalisation) != MPOptions::none )
+  if ((f & MPOptions::normalisation) != MPOptions::none)
     s << "	 - Normalisation is included\n";
   return s;
 }
-
+}  // namespace mutlipole_conv
 #endif
