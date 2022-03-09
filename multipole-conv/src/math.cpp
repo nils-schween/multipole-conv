595605dc1cc0203c7f529e855801b484bc80fc05
Nils Schween <nils.schween@mpi-hd.mpg.de>
2022-03-09 21:31:42 +0100

    implement factorial (and adaption because of new namespace)

--- multipole-conv/src/math.cpp
+++ multipole-conv/src/math.cpp
@@ -1,10 +1,19 @@
 #include "math.h"
 
-unsigned int binomial(unsigned int n, unsigned int k) {
-  unsigned r = 1.;
-  for (unsigned int d = 1; d <= k; ++d) {
+// Computes the binomial coefficient
+double multipole_conv::binomial(unsigned int n, unsigned int k) {
+  double r = 1.;
+  for (unsigned int d = 1.; d <= k; ++d) {
     r *= n--;
     r /= d;
   }
   return r;
+}
+
+// Computes n factorial
+double multipole_conv::factorial(unsigned int n) {
+  double res = (n > 1) ? n : 1;
+  if (n > 2)
+    for (unsigned int i = n - 1; i > 1; --i) res *= i;
+  return res;
 }
