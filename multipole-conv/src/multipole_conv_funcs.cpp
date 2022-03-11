caf0d81ea94af7f0545c6c5ed37494339e436a08
Nils Schween <nils.schween@mpi-hd.mpg.de>
2022-03-11 18:23:55 +0100

    fix errors in the permutation matrix

--- multipole-conv/src/multipole_conv_funcs.cpp
+++ multipole-conv/src/multipole_conv_funcs.cpp
@@ -128,26 +128,38 @@ multipole_conv::SquareMatrix<double> multipole_conv::basis_transformation(
   return basis_transformation;
 }
 
-multipole_conv::SquareMatrix<double> multipole_conv::permutation(size_t degree) {
+multipole_conv::SquareMatrix<double> multipole_conv::permutation(
+    size_t degree) {
   SquareMatrix<double> permutation_mat(2 * degree + 1);
+  // l
   // upper part: r^l Y_ll0, r^l Y_ll-20 ...
   // lower part: r^l Y_ll1, r^l Y_ll-21 ...
   size_t num_permutations_l = std::floor(degree / 2);
   permutation_mat(0, 0) = 1.;                    // first row upper part (s = 0)
   permutation_mat(degree + 1, 2 * degree) = 1.;  // firsr row lower part (s = 1)
   for (size_t i = 1; i <= num_permutations_l; ++i) {
-    permutation_mat(i, 2 * i) = 1.;                        // upper part
+    permutation_mat(i, 2 * i) = 1.;                            // upper part
+    // in the lower part (s = 1) Y_l01 does not exist
+    if ( degree % 2 == 0 && i == num_permutations_l)
+      continue;
     permutation_mat(degree + 1 + i, 2 * degree - 2 * i) = 1.;  // lower part
   }
+  // l-1
   // upper part: r^l Y_l-1l-10, r^l Y_l-1l-30 ...
   // lower part: r^l Y_l-1l-11, r^l Y_l-1l-31 ...
   size_t num_permutations_l_minus_one = std::floor((degree - 1) / 2);
+  // first row upper part
   permutation_mat(num_permutations_l + 1, 1) = 1.;
-  permutation_mat(degree + 1 + num_permutations_l + 1, 2 * degree - 1) = 1.;
+  // first row lower part
+  permutation_mat(degree + 1 + num_permutations_l_minus_one + 1,
+                  2 * degree - 1) = 1.;
   for (size_t i = 1; i <= num_permutations_l_minus_one; ++i) {
-    permutation_mat(num_permutations_l + 1 + i, 2 * i + 2) = 1.;
-    permutation_mat(degree + 1 + num_permutations_l + 1 + i,
-                    2 * degree - 1 - 2 * i) = 1.;
+    permutation_mat(num_permutations_l + 1 + i, 2 * i + 1) = 1.; // upper part
+    // in the lower part (s = 1) Y_l-101 does not exist
+    if ( degree % 2 == 1 && i == num_permutations_l_minus_one)
+      continue;
+    permutation_mat(degree + 1 + num_permutations_l_minus_one + 1 + i, 
+                    2 * degree - 1 - 2 * i) = 1.; // lower part
   }
   return permutation_mat;
 }
