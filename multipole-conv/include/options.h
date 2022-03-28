67ab36700ecacc4897618a25c15741dee7becf1f
Nils Schween <nils.schween@mpi-hd.mpg.de>
2022-03-28 15:36:20 +0200

    fix the set of options

--- multipole-conv/include/options.h
+++ multipole-conv/include/options.h
@@ -4,9 +4,19 @@
 namespace multipole_conv {
 enum class MPOptions {
   none = 1 << 0,
-  complex = 1 << 1,
-  normalisation = 1 << 2,
-  condon_shortley_phase = 1 << 3
+  complex = 1 << 1,                // use complex solid harmonics
+  normalisation = 1 << 2,          // normalise (real/complex) solid harmonics
+  remove_condon_shortley_phase = 1 << 3,  // remove the condon shortley phase
+  // include the addition theorem factor in the definition of the spherical
+  // multipole moments: (2l + 1)/4pi (with normalisation) and (2/(1 + \delta_m0)
+  // * (l - m)!/(l + m)! (without normalisation)
+  include_addition_theorem = 1 << 4,
+  split_addition_theroem = 1 << 5,  // Include the square root of the addition
+                                    // theorem factor in the definiton spherical
+                                    // multipole moments
+  cartesian = 1 << 6,               // cartesian mps in terms of spherical mps
+  include_l_factorial = 1 << 7,     //  Include l factorial in the definition of
+                                    //  the cartesian mps
 };
 
 constexpr MPOptions operator|(MPOptions f1, MPOptions f2) {
@@ -21,10 +31,31 @@ template <typename StreamType>
 inline StreamType &operator<<(StreamType &s, MPOptions f) {
   s << "Used options: \n";
   if ((f & MPOptions::complex) != MPOptions::none)
-    s << "	 - Complex spherical harmonics\n";
+    s << "	 - Complex solid harmonics are used\n";
   if ((f & MPOptions::normalisation) != MPOptions::none)
-    s << "	 - Normalisation is included\n";
+    s << "	 - The solid harmonics are normalised\n";
+  if ((f & MPOptions::remove_condon_shortley_phase) != MPOptions::none) {
+    s << "	 - The Condon-Shortley phase is removed from the spherical "
+         "harmonics\n";
+  }
+  if ((f & MPOptions::include_addition_theorem) != MPOptions::none) {
+    s << "	 - An additional factor coming from the addition theorem is "
+         "included in the definition of the spherical multipole moments\n";
+  }
+  if ((f & MPOptions::split_addition_theroem) != MPOptions::none) {
+    s << "	 - The square root of the additional factor coming from the "
+         "addition theorem is included in the definition of the spherical "
+         "multipole moments\n";
+  }
+  if ((f & MPOptions::cartesian) != MPOptions::none) {
+    s << "	 - The Cartesian multipole moments are expressed as a linear "
+         "combination of the spherical multipole moments.\n";
+  }
+  if ((f & MPOptions::include_l_factorial) != MPOptions::none) {
+    s << "	 - The l! from the Taylor expansion is included in the "
+         "defintion of the Cartesian multipole moments\n";
+  }
   return s;
 }
-}  // namespace mutlipole_conv
+}  // namespace multipole_conv
 #endif
