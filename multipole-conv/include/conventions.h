8461db15461860055e708998eb52ffe2650dac4f
Nils Schween <nils.schween@mpi-hd.mpg.de>
2022-03-31 15:16:54 +0200

    define a first set of conventions

---
+++ multipole-conv/include/conventions.h
@@ -0,0 +1,20 @@
+#ifndef CONVENTIONS_H
+#define CONVENTIONS_H
+#include "options.h"
+
+namespace multipole_conv {
+namespace conventions {
+constexpr MPOptions solid_harmonics =
+    MPOptions::complex | MPOptions::normalisation;
+
+constexpr MPOptions jackson = MPOptions::complex | MPOptions::normalisation |
+                              MPOptions::complex_conjugate;
+constexpr MPOptions johnston =
+    MPOptions::cartesian | MPOptions::include_addition_theorem |
+    MPOptions::remove_condon_shortley_phase | MPOptions::include_l_factorial |
+    MPOptions::dependent_components;
+
+constexpr MPOptions real_solid_harmonics = MPOptions::normalisation;
+}  // namespace conventions
+}  // namespace multipole_conv
+#endif
