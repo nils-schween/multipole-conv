ab10dbd09ce8874521dfb29a28c0e1de31c7f605
Nils Schween <nils.schween@mpi-hd.mpg.de>
2022-04-01 10:06:09 +0200

    add option convention and rename options -> cmd_options

--- multipole-conv/src/command_line_parser.cpp
+++ multipole-conv/src/command_line_parser.cpp
@@ -2,19 +2,39 @@
 #include <cmath>
 #include <cstddef>
 #include <iostream>
+#include <string>
 #include <utility>
 
+#include "conventions.h"
 #include "options.h"
 
 namespace multipole_conv {
+MPOptions set_convention(const std::string &convention) {
+  MPOptions options = MPOptions::none;
+  if (convention == "solid_harmonics")
+    options = conventions::solid_harmonics;
+  else if (convention == "jackson")
+    options = conventions::jackson;
+  else if (convention == "johnston")
+    options = conventions::johnston;
+  else if (convention == "real_solid_harmonics")
+    options = conventions::real_solid_harmonics;
+  else
+    std::cout << "No matching convention was found.\n";
+  return options;
+}
+
 std::pair<std::size_t, MPOptions> cmd_parser(int argc, char *argv[]) {
   using namespace boost::program_options;
-  std::pair<std::size_t, MPOptions> options{0, MPOptions::none};
+  std::pair<std::size_t, MPOptions> cmd_options{0, MPOptions::none};
   try {
     options_description desc{"Multipole Converter\nOptions"};
     desc.add_options()("help,h", "Help screen")(
         "degree,d", value<int>()->default_value(2),
         "Degree of spherical or Cartesian multipole moments.")(
+        "convention,c", value<std::string>(),
+        "Conventions are predefined set of options. Possible values are "
+        "solid_harmonics, jackson, johnston, real_solid_harmonics.")(
         "complex", value<bool>(), "Use complex solid harmonics.")(
         "complex-conjugate", value<bool>(),
         "Use the complex conjugate of the solid harmonics.")(
@@ -37,46 +57,52 @@ std::pair<std::size_t, MPOptions> cmd_parser(int argc, char *argv[]) {
 
     variables_map vm;
     store(parse_command_line(argc, argv, desc), vm);
-    // notify(vm);
+
     if (vm.count("help")) std::cout << desc << '\n';
     if (vm.count("degree")) {
       std::cout
           << "The degree of the (spherical/Cartesian) multipole moment is: "
           << vm["degree"].as<int>() << '\n';
-      options.first = static_cast<std::size_t>(vm["degree"].as<int>());
+      cmd_options.first = static_cast<std::size_t>(vm["degree"].as<int>());
     }
+    if (vm.count("convention"))
+      cmd_options.second = set_convention(vm["convention"].as<std::string>());
     if (vm.count("complex"))
       if (vm["complex"].as<bool>())
-        options.second = options.second | MPOptions::complex;
+        cmd_options.second = cmd_options.second | MPOptions::complex;
     if (vm.count("complex-conjugate"))
       if (vm["complex-conjugate"].as<bool>())
-        options.second = options.second | MPOptions::complex_conjugate;
+        cmd_options.second = cmd_options.second | MPOptions::complex_conjugate;
     if (vm.count("normalisation"))
       if (vm["normalisation"].as<bool>())
-        options.second = options.second | MPOptions::normalisation;
+        cmd_options.second = cmd_options.second | MPOptions::normalisation;
     if (vm.count("remove-csp"))
       if (vm["remove-csp"].as<bool>())
-        options.second =
-            options.second | MPOptions::remove_condon_shortley_phase;
+        cmd_options.second =
+            cmd_options.second | MPOptions::remove_condon_shortley_phase;
     if (vm.count("include-addition-thm"))
       if (vm["include-addition-thm"].as<bool>())
-        options.second = options.second | MPOptions::include_addition_theorem;
+        cmd_options.second =
+            cmd_options.second | MPOptions::include_addition_theorem;
     if (vm.count("split-addition-thm"))
       if (vm["split-addition-thm"].as<bool>())
-        options.second = options.second | MPOptions::split_addition_theorem;
+        cmd_options.second =
+            cmd_options.second | MPOptions::split_addition_theorem;
     if (vm.count("cartesian"))
       if (vm["cartesian"].as<bool>())
-        options.second = options.second | MPOptions::cartesian;
+        cmd_options.second = cmd_options.second | MPOptions::cartesian;
     if (vm.count("dependent-components"))
       if (vm["dependent-components"].as<bool>())
-        options.second = options.second | MPOptions::dependent_components;
+        cmd_options.second =
+            cmd_options.second | MPOptions::dependent_components;
     if (vm.count("include-l-factorial"))
       if (vm["include-l-factorial"].as<bool>())
-        options.second = options.second | MPOptions::include_l_factorial;
+        cmd_options.second =
+            cmd_options.second | MPOptions::include_l_factorial;
 
   } catch (const error &ex) {
     std::cerr << ex.what() << '\n';
   }
-  return options;
+  return cmd_options;
 }
 }  // namespace multipole_conv
