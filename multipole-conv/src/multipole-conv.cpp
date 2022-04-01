a752b1e171185f7aa8fb2d4c728af8fce4469126
Nils Schween <nils.schween@mpi-hd.mpg.de>
2022-04-01 10:07:55 +0200

    add information for no options set and adjust formating

--- multipole-conv/src/multipole-conv.cpp
+++ multipole-conv/src/multipole-conv.cpp
@@ -1,8 +1,8 @@
 #include <cmath>
 #include <complex>
 #include <iostream>
-#include <vector>
 #include <utility>
+#include <vector>
 
 #include "command_line_parser.h"
 #include "conventions.h"
@@ -20,29 +20,32 @@ int main(int argc, char *argv[]) {
   std::pair<std::size_t, MPOptions> cmd_line_arguments = cmd_parser(argc, argv);
   MPOptions options = cmd_line_arguments.second;
   std::size_t degree = cmd_line_arguments.first;
-  std::cout << "\n" << options << "\n";
-    
+  if (options == MPOptions::none)
+    std::cout << "No options were set. Default: Real solid harmonics (without "
+                 "normalisation) are computed.\n\n";
+  else
+    std::cout << options << "\n";
+
   if ((options & MPOptions::cartesian) != MPOptions::none) {  // CMPs
     if ((options & MPOptions::complex) != MPOptions::none) {
       Matrix<std::complex<double>> cmp =
           pipeline_cmp_cmplx_smp(options, degree);
       print_cartesian_multipole_moments(cmp, options);
-      if ((options & MPOptions::dependent_components) != MPOptions::none){
-	Matrix<std::complex<double>> dep_comps = dependent_components(cmp);
-	print_dependent_components(dep_comps, options);
+      if ((options & MPOptions::dependent_components) != MPOptions::none) {
+        Matrix<std::complex<double>> dep_comps = dependent_components(cmp);
+        print_dependent_components(dep_comps, options);
       }
     } else {
       Matrix<double> cmp = pipeline_cmp_real_smp(options, degree);
       print_cartesian_multipole_moments(cmp, options);
-      if ((options & MPOptions::dependent_components) != MPOptions::none){
-	Matrix<double> dep_comps = dependent_components(cmp);
-	print_dependent_components(dep_comps, options);
+      if ((options & MPOptions::dependent_components) != MPOptions::none) {
+        Matrix<double> dep_comps = dependent_components(cmp);
+        print_dependent_components(dep_comps, options);
       }
     }
   } else {  // SMPsp
     if ((options & MPOptions::complex) != MPOptions::none) {
-      Matrix<std::complex<double>> smp =
-          pipeline_cmplx_spm(options, degree);
+      Matrix<std::complex<double>> smp = pipeline_cmplx_spm(options, degree);
       print_spherical_multipole_moments(smp, options);
     } else {
       Matrix<double> smp = pipeline_real_smp(options, degree);
