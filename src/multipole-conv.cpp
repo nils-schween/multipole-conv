695398281f66187d00e20f80630b6d6f45a6693c
Nils Schween <nils.schween@mpi-hd.mpg.de>
2022-04-02 19:22:17 +0200

    add text when no option was entered

--- src/multipole-conv.cpp
+++ src/multipole-conv.cpp
@@ -20,36 +20,39 @@ int main(int argc, char *argv[]) {
   std::pair<std::size_t, MPOptions> cmd_line_arguments = cmd_parser(argc, argv);
   MPOptions options = cmd_line_arguments.second;
   std::size_t degree = cmd_line_arguments.first;
-  if (options == MPOptions::none)
+  if (options == MPOptions::none) {
+    std::cout << "Multipole converter.\nType \"multipole-conv -h\" to see the "
+                 "available options.";
     return 0;
-  else
+  } else {
     std::cout << options << "\n";
 
-  if ((options & MPOptions::cartesian) != MPOptions::none) {  // CMPs
-    if ((options & MPOptions::complex) != MPOptions::none) {
-      Matrix<std::complex<double>> cmp =
-          pipeline_cmp_cmplx_smp(options, degree);
-      print_cartesian_multipole_moments(cmp, options);
-      if ((options & MPOptions::dependent_components) != MPOptions::none) {
-        Matrix<std::complex<double>> dep_comps = dependent_components(cmp);
-        print_dependent_components(dep_comps, options);
+    if ((options & MPOptions::cartesian) != MPOptions::none) {  // CMPs
+      if ((options & MPOptions::complex) != MPOptions::none) {
+        Matrix<std::complex<double>> cmp =
+            pipeline_cmp_cmplx_smp(options, degree);
+        print_cartesian_multipole_moments(cmp, options);
+        if ((options & MPOptions::dependent_components) != MPOptions::none) {
+          Matrix<std::complex<double>> dep_comps = dependent_components(cmp);
+          print_dependent_components(dep_comps, options);
+        }
+      } else {
+        Matrix<double> cmp = pipeline_cmp_real_smp(options, degree);
+        print_cartesian_multipole_moments(cmp, options);
+        if ((options & MPOptions::dependent_components) != MPOptions::none) {
+          Matrix<double> dep_comps = dependent_components(cmp);
+          print_dependent_components(dep_comps, options);
+        }
       }
-    } else {
-      Matrix<double> cmp = pipeline_cmp_real_smp(options, degree);
-      print_cartesian_multipole_moments(cmp, options);
-      if ((options & MPOptions::dependent_components) != MPOptions::none) {
-        Matrix<double> dep_comps = dependent_components(cmp);
-        print_dependent_components(dep_comps, options);
+    } else {  // SMPsp
+      if ((options & MPOptions::complex) != MPOptions::none) {
+        Matrix<std::complex<double>> smp = pipeline_cmplx_spm(options, degree);
+        print_spherical_multipole_moments(smp, options);
+      } else {
+        Matrix<double> smp = pipeline_real_smp(options, degree);
+        print_spherical_multipole_moments(smp, options);
       }
     }
-  } else {  // SMPsp
-    if ((options & MPOptions::complex) != MPOptions::none) {
-      Matrix<std::complex<double>> smp = pipeline_cmplx_spm(options, degree);
-      print_spherical_multipole_moments(smp, options);
-    } else {
-      Matrix<double> smp = pipeline_real_smp(options, degree);
-      print_spherical_multipole_moments(smp, options);
-    }
+    return 0;
   }
-  return 0;
 }
